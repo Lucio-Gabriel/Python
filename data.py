2db081872fcb3442b2be31d1cf346b0f31c6954c
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-08-14 18:50:54 -0300

    start commit

---
+++ data.py
@@ -0,0 +1,16 @@
+# PythonFlix
+
+name = "Superman";
+yearLaunch = 2025;
+noteMovie = 8.0;
+planInclude = False;
+
+print("Nome do filme:", name);
+print("Ano:", yearLaunch);
+print("Nota:", noteMovie);
+print("Está incluso no plano:", planInclude);
+
+print(type(name));
+print(type(yearLaunch));
+print(type(noteMovie));
+print(type(planInclude));
