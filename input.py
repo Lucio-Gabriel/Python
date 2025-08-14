2adc37c84f687dcd5683e3cd7a0fcc7ed1c5abf8
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-08-14 19:13:37 -0300

    input and typage

---
+++ input.py
@@ -0,0 +1,13 @@
+# Utilizando o Input
+
+name = input("Digite o nome do filme: \n");
+yearLaunch = int(input("Digite o ano de lançamento: \n"));
+noteMovie = float(input("Digite a nota do filme: \n"));
+
+print(name);
+print(yearLaunch);
+print(noteMovie);
+
+print(type(name));
+print(type(yearLaunch));
+print(type(noteMovie));
