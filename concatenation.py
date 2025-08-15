93d8ccef607c8ce6fee76cbcdd220a8ce352de9d
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-08-15 10:07:55 -0300

    concatenations and operations

---
+++ concatenation.py
@@ -0,0 +1,11 @@
+name = input("Digite o nome do filme: \n");
+yearLaunch = int(input("Digite o ano de lançamento: \n"));
+noteMovie = float(input("Digite a nota do filme: \n"));
+
+print("Dados do filme");
+print("==========================");
+
+print(f"Nome do filme: {name}\n"
+      f"Ano do lançamento: {yearLaunch}\n"
+      f"Nota do filme: {noteMovie}\n"
+     );
